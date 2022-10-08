01987e6b8a7ea785c233ba919f5465922f184636
M. Corey Glass <dhexcxr@gmail.com>
2022-10-08 10:26:49 -0400

    refactor:remove unused code

--- ListerWeb/src/main/java/meprod/ListerWeb/MainMenu.java
+++ ListerWeb/src/main/java/meprod/ListerWeb/MainMenu.java
@@ -81,45 +81,7 @@ public class MainMenu extends HttpServlet {
 			rd.forward(request, response);
 			return;
 		}
-
-		if (request.getParameter("show_lists") != null){
-			// TODO i dont think this is being called from anywhere
-			System.out.println("show list func start");
-			
-			List<ToDoList> listerLists = HiberFunc.getLists();
-			if (listerLists == null) {
-				out.println("Database error...\n");
-			} else {
-				System.out.println("showList func - List of Lists: " + listerLists.get(0).toString());
-				request.setAttribute("listerLists", listerLists);
-				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-				rd.forward(request, response);
-			}
-			return;
-		}
 		
-		if (request.getParameter("create_list") != null) {
-			
-		}
-
-//		if(request.getParameter("updateEmployee")!=null){
-//			int id1 = Integer.parseInt(request.getParameter("id"));
-//			String ename = request.getParameter("ename");
-//			String enumberupdate = request.getParameter("enumberupdate");
-//			employeeDaoImpl.updateEmployee(id1, ename, enumberupdate);
-//
-//			RequestDispatcher rd = request.getRequestDispatcher("EmployeeAdd.jsp");
-//			rd.forward(request, response);
-
-//		}
-
-//		if(request.getParameter("deleteEmployee")!=null){
-//			int id2 = Integer.parseInt(request.getParameter("id"));
-//			employee.setId(id2);
-//			employeeDaoImpl.deleteEmployee(employee);
-//			RequestDispatcher rd = request.getRequestDispatcher("EmployeeAdd.jsp");
-//			rd.forward(request, response);
-//		}
 		this.doGet(request, response);
 	}
 
